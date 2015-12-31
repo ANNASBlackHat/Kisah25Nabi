9b0b820ee7cf2483805bfabfc0dadea9bd95f624
ANNASBlackHat <annas685@gmail.com>
2015-12-31 11:24:07 +0700

    update

--- app/src/main/java/com/example/annasblackhat/kisah25nabi/KisahDetail.java
+++ app/src/main/java/com/example/annasblackhat/kisah25nabi/KisahDetail.java
@@ -2,12 +2,16 @@ package com.example.annasblackhat.kisah25nabi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class KisahDetail extends AppCompatActivity {
 
     public static final String KEY_NABI = "namaNabi";
     public static final String KEY_KISAH = "kisahNabi";
+    public static final String KEY_IMAGE = "gambar";
+    int x = R.drawable.background;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +20,10 @@ public class KisahDetail extends AppCompatActivity {
 
         String nama = getIntent().getStringExtra(KEY_NABI);
         String kisah = getIntent().getStringExtra(KEY_KISAH);
+        int gambar = getIntent().getIntExtra(KEY_IMAGE, x);
 
         ((TextView)findViewById(R.id.nama_nabi)).setText(nama);
-        ((TextView)findViewById(R.id.txt_kisah)).setText(kisah);
+        ((TextView)findViewById(R.id.txt_kisah)).setText(Html.fromHtml(kisah));
+        ((ImageView)findViewById(R.id.gbr)).setImageResource(gambar);
     }
 }
--- app/src/main/java/com/example/annasblackhat/kisah25nabi/MainActivity.java
+++ app/src/main/java/com/example/annasblackhat/kisah25nabi/MainActivity.java
@@ -25,10 +25,14 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, KisahDetail.class);
                 intent.putExtra(KisahDetail.KEY_NABI, nabi[0][position]);
                 intent.putExtra(KisahDetail.KEY_KISAH, nabi[1][position]);
+                intent.putExtra(KisahDetail.KEY_IMAGE, gambar[position]);
                 startActivity(intent);
             }
         });
+
     }
+
+    int[] gambar = {R.drawable.steve};
 
     String[][] nabi = {
             {"Nabi Adam As.","Nabi Idris As.","Nabi Nuh As.","Nabi Hud As.","Nabi Sholeh As.",
@@ -38,11 +42,39 @@ public class MainActivity extends AppCompatActivity {
                     "Nabi Sulaiman As.","Nabi Ilyas As.","Nabi Ilyasa As.","Nabi Yunus As.",
                     "Nabi Zakaria As.","Nabi Yahya As.","Nabi Isa As.","Nabi Muhammad Saw"
             },
-            {"Ini Kisah Nabi Adam As.","Ini Kisah Nabi Idris As.","Ini Kisah Nabi Nuh As.","Ini Kisah Nabi Hud As.","Ini Kisah Nabi Sholeh As.",
-                    "Ini Kisah Nabi Ibrohim As.","Ini Kisah Nabi Luth As.","Ini Kisah Nabi Ismail As.","Ini Kisah Nabi Ishak As.",
-                    "Ini Kisah Nabi Ya’kub As.","Ini Kisah Nabi Yusuf As.","Ini Kisah Nabi Ayub As.","Ini Kisah Nabi Syu’aib As.",
-                    "Ini Kisah Nabi Musa As.","Ini Kisah Nabi Harun As.","Ini Kisah Nabi Dzulkifli As.","Ini Kisah Nabi Daud As.",
-                    "Ini Kisah Nabi Sulaiman As.","Ini Kisah Nabi Ilyas As.","Ini Kisah Nabi Ilyasa As.","Ini Kisah Nabi Yunus As.",
+            {"Allah Subhanahu wa Ta’ala memberitahukan kepada para malaikat tentang penciptaan Adam ‘alaihis salam, Dia berfirman:<br/>" +
+                    "<br/>" +
+                    "<b>“Sesungguhnya aku hendak menjadikan seorang khalifah di muka bumi.”</b> (QS. Al Baqarah: 30)<br/><br/>" +
+                    "<br/>" +
+                    "Yakni makhluk yang satu dengan yang lain saling menggantikan. Demikianlah Allah Subhanahu wa Ta’ala memberitahukan kepada para malaikat tentang penciptaan Adam sebagaimana Dia memberitahukan perkara besar sebelum terwujud.<br/>" +
+                    "<br/>" +
+                    "Kemudian para malaikat bertanya kepada Allah Subhanahu wa Ta’ala meminta diterangkan hikmah diciptakannya manusia, karena para malaikat mengetahui bahwa di antara manusia ada yang membuat kerusakan di bumi dan menumpahkan darah. Menurut Qatadah, mereka mengetahui demikian karena mereka melihat makhluk sebelum Adam, yaitu jin dan Hin (sekelompok jin atau golongan jin yang lemah). Menurut Ibnu Umar, dua ribu tahun sebelum Adam diciptakan, jin sudah ada (menempati bumi), lalu mereka menumpahkan darah, maka Allah Subhanahu wa Ta’ala mengutus satu pasukan malaikat, lalu mereka mengusirnya ke jazirah laut.”<br/>" +
+                    "<br/>" +
+                    "Menurut para malaikat, jika hikmah diciptakannya manusia adalah untuk beribadah kepada Allah, maka sesungguhnya mereka telah beribadah kepada-Nya, mereka berkata,<br/>" +
+                    "<br/>" +
+                    "“Mengapa Engkau hendak menjadikan (khalifah) di bumi itu orang yang akan membuat kerusakan padanya dan menumpahkan darah, padahal Kami senantiasa bertasbih dengan memuji Engkau dan mensucikan Engkau?” Allah Subhanahu wa Ta’ala berfirman, “Sesungguhnya Aku mengetahui apa yang tidak kamu ketahui.” (QS. Al Baqarah: 30)<br/>" +
+                    "<br/>" +
+                    "Dia mengetahui maslahat yang lebih kuat dengan menciptakan Adam dan keturunannya, karena akan ada di antara mereka yang menjadi para nabi dan rasul, para shiddiqin, para syuhada, para ulama dan orang-orang yang mengamalkan agama-Nya, yang mencintai-Nya, dan mengikuti para rasul-Nya.<br/>" +
+                    "<br/>" +
+                    "Allah Subhanahu wa Ta’ala menciptakan Adam ‘alaihis salam dari tanah di bumi dan airnya, lalu membentuknya dengan bentuk yang sebaik-baiknya, kemudian Dia tiupkan ruh ke dalamnya, maka jadilah dia sebagai manusia yang hidup yang terdiri dari daging, darah, dan tulang. Hari penciptaan Adam ‘alaihis salam adalah hari Jumat, Rasulullah shallallahu ‘alaihi wa sallam bersabda,",
+                    "Ini Kisah Nabi Idris As.",
+                    "Ini Kisah Nabi Nuh As.",
+                    "Ini Kisah Nabi Hud As.",
+                    "Ini Kisah Nabi Sholeh As.",
+                    "Ini Kisah Nabi Ibrohim As.",
+                    "Ini Kisah Nabi Luth As.",
+                    "Ini Kisah Nabi Ismail As.",
+                    "Ini Kisah Nabi Ishak As.",
+                    "Ini Kisah Nabi Ya’kub As.",
+                    "Ini Kisah Nabi Yusuf As.",
+                    "Ini Kisah Nabi Ayub As.",
+                    "Ini Kisah Nabi Syu’aib As.",
+                    "Ini Kisah Nabi Musa As.",
+                    "Ini Kisah Nabi Harun As.",
+                    "Ini Kisah Nabi Dzulkifli As.",
+                    "Ini Kisah Nabi Daud As.",
+                    "Ini Kisah Nabi Sulaiman As.",
+                    "Ini Kisah Nabi Ilyas As.","Ini Kisah Nabi Ilyasa As.","Ini Kisah Nabi Yunus As.",
                     "Ini Kisah Nabi Zakaria As.","Ini Kisah Nabi Yahya As.","Ini Kisah Nabi Isa As.","Ini Kisah Nabi Muhammad Saw"}
     };
 }
